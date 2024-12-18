88ee443976ab10941acd151d6bc2efd9c906b9db
Jin Junjie <jinjj@chinasatnet.com.cn>
2024-12-18 11:10:27 +0800

    update

--- src/login.ts
+++ src/login.ts
@@ -11,12 +11,12 @@ const db = new sqlite3.Database('chat.db')
 router.post("/", (req: Request, res: Response) => {
 
   let username = req.body.username ?? ''
-  let password = req.body.password ?? ''
+  // let password = req.body.password ?? ''
   let roomId = req.body.roomId ?? '' 
   let optToken = req.body.token ?? ''
   
   
-  db.get('select * from users where username = ? and password = ?', [username, password], (err, user_data: any) => {
+  db.get('select * from users where username = ? and password = ?', username, (err, user_data: any) => {
     if (err) {
       console.error('内部错误:', err)
       res.status(500).send('内部错误')
